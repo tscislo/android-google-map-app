09a23bd093023b5be7d0ceb5ded110557c32b0ff
Tomasz Ścisło <scislot@gmail.com>
2019-03-17 14:55:42 +0100

    two way data-binding

--- app/src/main/java/eu/scislo/mobilenext/MainActivity.java
+++ app/src/main/java/eu/scislo/mobilenext/MainActivity.java
@@ -1,17 +1,26 @@
 package eu.scislo.mobilenext;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import eu.scislo.mobilenext.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    User user = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setUser(this.user);
     }
 
     public void navigateToMap(View view) {
@@ -21,9 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void navigateMessageRead(View view) {
         Intent intent = new Intent(this, MessageRead.class);
-        TextView editText = findViewById(R.id.name);
-        String message = editText.getText().toString();
-        intent.putExtra(MyIntents.MESSAGE.toString(), message);
+        intent.putExtra(MyIntents.MESSAGE.toString(), this.user.toString());
         startActivity(intent);
     }
 
